03cfe01c7fa05035cbc18539645e49375b2d04c5
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-11 17:44:01 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -7,7 +7,7 @@
 # "LiberTeens" => '/home/rational-parameter-4800/jukebox-cli/audio/Emerald-Park/02.mp3',
 # "Hamburg" =>  '/home/rational-parameter-4800/jukebox-cli/audio/Emerald-Park/03.mp3',
 # "Guiding Light" => '/home/rational-parameter-4800/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => 'Users/home/rational-parameter-4800/jukebox-cli/audio/Emerald-Park/05.mp3',
+# "Wolf" => '/Users/home/rational-parameter-4800/jukebox-cli/audio/Emerald-Park/05.mp3',
 # "Blue" => '/home/rational-parameter-4800/jukebox-cli/audio/Emerald-Park/06.mp3',
 # "Graduation Failed" => '/home/rational-parameter-4800le/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
